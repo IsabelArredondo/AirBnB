7970eeb327f3cbfeb8627f0b74de9a00988d679a
Isabel Arredondo <isabelarredondo@Isabels-Air.attlocal.net>
2022-07-10 20:04:22 -0500

    error update reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const { requireAuth } = require('../../utils/auth');
 const { Review, Image, Spot, User } = require('../../db/models');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+//const { check } = require('express-validator');
+//const { handleValidationErrors } = require('../../utils/validation');
 const { Op } = require('sequelize');
 
 
