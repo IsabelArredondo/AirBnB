e355b8eca7b3a3f0f49ae318eae4be4d8ffb4be2
Isabel Arredondo <isabelarredondo@Isabels-Air.attlocal.net>
2022-07-10 19:47:54 -0500

    error update reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -146,7 +146,7 @@ const error = {
 }
 
 //Update and return an existing review.
-router.put('/:id', requireAuth, validateSpots, async (req, res) => {
+router.put('/:id', requireAuth, async (req, res) => {
 
   const { review, stars } = req.body
   const reviews = await Review.findOne({ where: { id: req.params.id}});
