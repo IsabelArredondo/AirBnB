6b47575c34e929605f1d293a3f0dea408786848a
Isabel Arredondo <isabelarredondo@isabels-air.attlocal.net>
2022-07-22 11:28:00 -0500

    phase 3 -complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -53,6 +53,13 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
 
 const initialState = { user: null };
 
