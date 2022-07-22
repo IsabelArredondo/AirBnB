d48a9dcb95603044b18e5ba3fb743890b8ce1da2
Isabel Arredondo <isabelarredondo@isabels-air.attlocal.net>
2022-07-22 11:54:53 -0500

    Bonus Phase -complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignUpFormPage";
+import SignUpFormPage from "./components/SignUpFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -19,11 +18,8 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
-            <SignupFormPage />
+            <SignUpFormPage />
           </Route>
         </Switch>
       )}
