c753ee88886a7a0e43e433e3dee9fd7e2376ce54
IsabelArredondo <izzyaround@gmail.com>
2023-06-30 14:48:45 -0500

    reviews.js - review css

--- frontend/src/components/SpotById/reviews.js
+++ frontend/src/components/SpotById/reviews.js
@@ -51,18 +51,21 @@ const Reviews = ({spotId, spots}) => {
                    </div>
                   
                 <div className="userStars">
-                <svg className="profile" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', margin: '1px', height: '100%', width: '100%', fill: 'currentcolor' }} width={30} height={30}><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z" fill="#717171" /></svg>
-
+                <i id='individualreview' class="fa-solid fa-circle-user"></i>
                 <span className='reviewuser'>{reviewState?.firstName} {reviewState?.lastName}</span>
                 {/* <div className="raitng">
                    <div className="star">{<i className="fas fa-star"></i>} </div>
                    <span className="starspace" >  </span>
                    <span className='stars'>{`${reviewState?.stars}`} </span>
                 </div> */}
+                
                 </div>
-
+                <div className='actual-review'>
+                  <p className='actual-review'>{reviewState?.review} </p>
+                </div>
+                
                   <div>
-                    <p className='actual-review'>{reviewState?.review} </p>
+                    
 
                     {currentUser &&
                      currentUser?.id === reviewState?.userId && (
